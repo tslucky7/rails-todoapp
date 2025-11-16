a7f3c81dd93d6a78f82c208fd9848241f2f508b3
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-11-16 23:37:25 +0900

    feat: ボード投稿機能を実装
    バックエンド側
    - routes.rbにboardsのルーティングを追加: [boardsの新規投稿、詳細表示、編集、削除を行うルーティングを追加]
    - boards_controller
      - createアクションを追加: [記事を投稿した際にデータを保存できる機能]
      - showアクションを追加: [記事の詳細を表示する機能（フロント側の実装はまだ）]
      - newアクションを追加: [記事を投稿するフォームを表示する機能, フロント実装完了]
      - strong_parametersのboard_paramsを追加: [記事を投稿する際に必要なデータのみを指定]
    - board.rbにバリデーションを追加: [name, descriptionが必須]
    フロントエンド側
    - tasks/index.html.haml: [記事投稿へのリンクを追加]
    - boards/new.html.hamlを追加: [記事投稿フォームを表示するビューを追加]
    - boards/show.html.hamlを追加: [表示用、フロントの実装はまだ]
    - scss: textarea用のスタイルを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,25 @@
 class BoardsController < ApplicationController
+  def create
+    @board = Board.new(board_params)
+    @board.user = current_user
+    if @board.save
+      redirect_to @board, notice: "Board was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  private
+
+    def board_params
+      params.require(:board).permit(:name, :description)
+    end
 end
