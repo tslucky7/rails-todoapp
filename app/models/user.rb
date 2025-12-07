d4e96c38c03fc4da5922aed378dc7f1ea700f3bc
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-12-07 17:33:09 +0900

    feat: タスク詳細にコメント一覧表示を追加・ユーザーアイコンを登録できるよう実装
    - application_controller.rb: deviseのパラメータ許可設定を追加し、ユーザーアイコンを登録できるようにした
    - tasks_controller.rb: タスク詳細にコメント一覧を表示するようにした
    - user.rb: ユーザーアイコンを登録できるようにした
    - devise/registrations/new.html.haml: ユーザーアイコンを登録できるようにした
    - layouts/application.html.haml: ユーザーアイコンを表示するようにした
    - tasks/index.html.haml: ユーザーアイコンを表示するようにした
    - tasks/show.html.haml: ユーザーアイコンを表示するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :icon
 
   def has_written?(board)
     boards.exists?(id: board.id)
