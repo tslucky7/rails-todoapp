d348a8ddac9a32279e07cab8e8f8d1b9062b4429
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-12-07 17:36:28 +0900

    fix: rubocopの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:icon])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:icon])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :icon ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :icon ])
   end
 end
